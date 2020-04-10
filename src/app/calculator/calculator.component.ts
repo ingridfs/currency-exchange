ee234e6f966347e47218cb537807ab4810cee4bd
ingridfs <ingrid.fernandes.santos@hotmail.com>
2020-04-09 21:39:27 -0300

    Adding currency flags and css ajusts for flags

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -22,7 +22,7 @@ export class CalculatorComponent implements OnInit {
   ngOnInit() {
     this.currency.getCurrency().subscribe(
         (response) => {
-        this.currencyExchanges = new Object (response);
+        this.currencyExchanges = new Object(response);
         this.currencyRate = this.currencyExchanges.rates['USD'].toFixed(2);
         this.currencySign = 'USD';
         }
@@ -30,27 +30,19 @@ export class CalculatorComponent implements OnInit {
   }
 
   selectedCurrency(coin){
-    console.log(coin);
     this.currencyRate = this.currencyExchanges.rates[coin].toFixed(2);
-    console.log(this.currencyRate);
     this.currencySign = coin;
-    console.log(coin);
-    
   }
 
   realCurrencyCalc(event){ 
     this.amount = Number(event.target.value);
-    console.log(this.amount);
     this.resultReal = this.amount * this.currencyRate;
-    console.log(this.resultReal);
     return this.resultReal.toFixed(2);
   }
   
   currencyRealCalc(event){ 
     this.amount = Number(event.target.value);
-    console.log(this.amount);
     this.resultCurrency = this.amount / this.currencyRate;
-    console.log(this.resultCurrency);
     return this.resultCurrency.toFixed(2);
   }
     
